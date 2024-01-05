0111be7a127566172e6816550b135b3eff16fb40
Jesper Kristensen <cableman@linuxdev.dk>
2024-01-05 16:35:18 +0100

    ITKDev: Updated to match docker setup

--- infrastructure/docker/run.sh
+++ infrastructure/docker/run.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+# Exit when any command fails
+set -e
+set -x
+
 APP_VERSION=develop
-VERSION=alpha
+VERSION=latest
 
-docker build --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=danskernesdigitalebibliotek/smart-search-service:${VERSION} --file="smart-search-service/Dockerfile" smart-search-service
-docker build --no-cache --build-arg VERSION=${VERSION} --tag=danskernesdigitalebibliotek/smart-search-service-nginx:${VERSION} --file="nginx/Dockerfile" nginx
+docker build --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=itkdev/smart-search-service:${VERSION} --file="smart-search-service/Dockerfile" smart-search-service
+docker push itkdev/smart-search-service:${VERSION}
 
-docker push danskernesdigitalebibliotek/smart-search-service:${VERSION}
-docker push danskernesdigitalebibliotek/smart-search-service-nginx:${VERSION}
+docker build --no-cache --build-arg VERSION=${VERSION} --tag=itkdev/smart-search-service-nginx:${VERSION} --file="nginx/Dockerfile" nginx
+docker push itkdev/smart-search-service-nginx:${VERSION}
